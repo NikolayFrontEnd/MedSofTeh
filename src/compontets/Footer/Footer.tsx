1df7ccb5f9cd5deee4f98f32af61c92d66344f42
NikolayFrontEnd <nikolaydon2@mail.ru>
2025-08-26 13:23:59 +0300

    добавил картинку

--- src/compontets/Footer/Footer.tsx
+++ src/compontets/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from './Footer.module.css';
 import phot from "../../assets/element2.png";
+import flag from "../../assets/ff.jpg"
 const Footer = () => {
     
     return (
@@ -11,9 +12,10 @@ const Footer = () => {
                 <span>М</span>
             </div>
             <span className={styles.logoText2}>МЕДСОФТЕХ</span>
+            <img className = {styles.photoRusFlag}src = {flag}/>
         </div>
 
-<div className={styles.desc}>Компания разрабатывает веб-приложение, упрощающие работу врачей и взаимодействие с пациентами.</div>
+<div className={styles.desc}>Проект создан при поддержке Федерального государственного бюджетного учреждения "Фонд содействия развитию малых форм предприятий в научно-технической сфере в рамках программы "Студенческий стартап" федерального проекта "Платформа университетского технологического предпринимательства".</div>
 <div className = {styles.desc}>Связаться с нами: sseed932@gmail.com</div>
 <div className = {styles.ourComp}>©Медсофтех 2025. Все права защищены</div>
 <img className = {styles.phot} src = {phot}/>
